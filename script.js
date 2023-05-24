bcd59e2218022765c94a5ff48ad84262d7121595
rmdhnz <ramadhanfadhly2@gmail.com>
2023-05-25 06:22:11 +0700

    menambah banner design

--- script.js
+++ script.js
@@ -16,11 +16,18 @@ $.ajax({
             setTimeout(() => {
               lulus.classList.toggle("d-none");
               document.querySelector(".jumbotron").classList.toggle("d-none");
+              document.querySelector(
+                ".pesan-lolos"
+              ).innerHTML = `Congratulations ${staffLulus[i]["Nama"]}, you are selected to be the part of us. Let's join our communal group at <a class="btn btn-primary" href='https://line.me/R/ti/g/WUAwSZo9vg' target='_blank'>here</a>. Also join our Welcome party at papercup manyar on 29th Mei 2023 at 07:00 P.M`;
             }, 3000);
           } else {
             setTimeout(() => {
               gagal.classList.toggle("d-none");
               document.querySelector(".jumbotron").classList.toggle("d-none");
+              document.querySelector(
+                ".pesan-gagal"
+              ).innerHTML = `Sorry ${staffLulus[i]["Nama"]} ! You are declared not passed to become IEEE staff. Keep the
+              spirit and good luck on another occasion`;
             }, 3000);
           }
           return;
