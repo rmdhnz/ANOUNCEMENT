7c30999838ccd1f68077d816105a8275112611d9
rmdhnz <ramadhanfadhly2@gmail.com>
2023-05-24 22:06:09 +0700

    menambah efek timeout

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-// const data = "data.json";
-
-// function getDataMahasiswa(url, success, error) {
-//   let xhr = new XMLHttpRequest();
-
-//   xhr.onreadystatechange = function () {
-//     if (xhr.readyState === 4) {
-//       if (xhr.status === 200) {
-//         success(xhr.response);
-//       } else if (xhr.status === 404) {
-//         error();
-//       }
-//     }
-//   };
-//   xhr.open("get", url);
-//   xhr.send();
-// }
-
-// getDataMahasiswa("data.json", (result) => {
-//   console.log(result);
-// });
-
 $.ajax({
   url: "staff_ieee.json",
   success: (res) => {
     let staffLulus = res;
     $("#form").on("submit", (e) => {
       e.preventDefault();
+      let lulus = document.querySelector(".lulus"),
+        gagal = document.querySelector(".gagal"),
+        btnLoading = document.querySelector(".btn-loading"),
+        btnKirim = document.querySelector(".btn-kirim");
       for (let i = 0; i < staffLulus.length; i++) {
         if ($(".nrp").val() == staffLulus[i]["NRP"]) {
-          console.log(`Selamat ${staffLulus[i]["Nama"]} anda dinyatakan lulus`);
+          btnKirim.classList.toggle("d-none");
+          btnLoading.classList.toggle("d-none");
+          if (staffLulus[i]["Position"] != "none") {
+            setTimeout(() => {
+              lulus.classList.toggle("d-none");
+              document.querySelector(".jumbotron").classList.toggle("d-none");
+            }, 3000);
+          } else {
+            setTimeout(() => {
+              gagal.classList.toggle("d-none");
+              document.querySelector(".jumbotron").classList.toggle("d-none");
+            }, 3000);
+          }
           return;
         }
       }
-      console.log("tidak lulus");
+      alert("Your NRP is not available");
+      $("#form").trigger("reset");
     });
   },
   error: (err) => {
